2776e857199239924cd3ba9dfe353fc38ee4e3aa
wangqingyi <2365209663@qq.com>
2024-05-21 17:23:15 +0800

    【info】merchantDao书写

--- src/main/java/dao/MerchantDao/MerchantDao.java
+++ src/main/java/dao/MerchantDao/MerchantDao.java
@@ -1,4 +1,24 @@
 package dao.MerchantDao;
 
+import pojo.Merchant;
+
+import java.util.ArrayList;
+
 public interface MerchantDao {
+    /**
+     * 获取商户名搜索出来
+     *
+     * @return
+     */
+    public ArrayList<Merchant> getSimpleMerchantByName(String name);
+
+    public Merchant getDetailedMerchantByName();
+
+    public boolean addMerchant();
+
+    public boolean deleteMerchantById(int id);
+
+    public int modifyMerchantById(Merchant merchant);
+
+    public Merchant getMerchantById(int id);
 }
--- src/main/java/dao/MerchantDao/MerchantDaoImpl.java
+++ src/main/java/dao/MerchantDao/MerchantDaoImpl.java
@@ -1,4 +1,41 @@
 package dao.MerchantDao;
 
+import pojo.Merchant;
+
+import java.util.ArrayList;
+
 public class MerchantDaoImpl implements MerchantDao{
+    @Override
+    public ArrayList<Merchant> getSimpleMerchantByName(String name) {
+        /**
+         * 根据用户输入的商户信息查询所有重名的商家
+         */
+
+        return null;
+    }
+
+    @Override
+    public Merchant getDetailedMerchantByName() {
+        return null;
+    }
+
+    @Override
+    public boolean add() {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public int modify(Merchant merchant) {
+        return 0;
+    }
+
+    @Override
+    public Merchant getMerchantById() {
+        return null;
+    }
 }
