30244cb1129f376bb3161811bdbbf58d456d2ca9
wangqingyi <2365209663@qq.com>
2024-05-28 16:59:50 +0800

    [info]实现分页查询功能中

--- src/main/java/servlet/User/userServlet.java
+++ src/main/java/servlet/User/userServlet.java
@@ -8,6 +8,7 @@ import service.Login.LoginServiceImpl;
 import service.User.UserService;
 import service.User.UserServiceImpl;
 import utils.Constants;
+import utils.PageSupport;
 import utils.Session;
 
 import javax.servlet.ServletException;
@@ -39,11 +40,73 @@ public class userServlet extends HttpServlet {
     }
 
     private void adminManage(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
-       //得到整个用户的列表
-        UserService userService=new UserServiceImpl();
-        ArrayList<User> userList=userService.getAllUserList();
-        request.setAttribute("userList",userList);
-        request.getRequestDispatcher(url).forward(request,response);
+       //得到整个用户的列表，实现分页查询
+
+        // todo 以下是原代码
+//        UserService userService=new UserServiceImpl();
+//        ArrayList<User> userList=userService.getAllUserList();
+//        request.setAttribute("userList",userList);
+//        request.getRequestDispatcher(url).forward(request,response);
+
+        //以下是参考代码
+            //查询用户列表
+
+            //获取分页查询下标
+            String pageIndex = request.getParameter("pageIndex");
+
+            UserService userService = new UserServiceImpl();
+
+            //第一次走页面一定是第一页,页面大小固定的
+            ArrayList<User> userList = null;
+            //设置页面容量
+            int pageSize = Constants.pageSize;
+            //当前页码
+            int currentPageNo = 1;
+
+            System.out.println("query pageIndex--------- > " + pageIndex);
+
+            if(pageIndex != null){
+                try{
+                    currentPageNo = Integer.valueOf(pageIndex);
+                }catch(NumberFormatException e){
+                    response.sendRedirect("error.jsp");
+                }
+            }
+            //总数量（表）
+            ArrayList<User> users=userService.getAllUserList();
+            int totalCount=users.size();
+            //总页数
+            PageSupport pages=new PageSupport();
+
+            pages.setCurrentPageNo(currentPageNo);
+
+            pages.setPageSize(pageSize);
+
+            pages.setTotalCount(totalCount);
+
+            int totalPageCount = pages.getTotalPageCount();
+
+            //控制首页和尾页
+            if(currentPageNo < 1){
+                currentPageNo = 1;
+            }else if(currentPageNo > totalPageCount){
+                currentPageNo = totalPageCount;
+            }
+
+
+//            userList = userService.getUserList(queryUserName,queryUserRole,currentPageNo, pageSize);
+//            request.setAttribute("userList", userList);
+//            List<Role> roleList = null;
+//            RoleService roleService = new RoleServiceImpl();
+//            roleList = roleService.getRoleList();
+//            request.setAttribute("roleList", roleList);
+//            request.setAttribute("queryUserName", queryUserName);
+//            request.setAttribute("queryUserRole", queryUserRole);
+//            request.setAttribute("totalPageCount", totalPageCount);
+//            request.setAttribute("totalCount", totalCount);
+//            request.setAttribute("currentPageNo", currentPageNo);
+//            request.getRequestDispatcher("userlist.jsp").forward(request, response);
+
     }
 
     private void getUserById(HttpServletRequest request, HttpServletResponse response, String url) throws ServletException, IOException {
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,5 +1,6 @@
 package utils;
 
 public interface Constants {
+        public final static int pageSize = 5;
         public final static String USER_SESSION = "userSession";
 }
